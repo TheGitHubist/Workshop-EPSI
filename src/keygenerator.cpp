612c3950807f04f292758b317ee0e49dac3ac215
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-09-22 15:22:45 +0200

    basic cpp

---
+++ src/keygenerator.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <ctime>
+
+enum LogicDoor {
+  XOR,
+  NOR,
+  NAND,
+  AND
+};
+
+std::string generateID() {
+  std::string id = "";
+  srand((unsigned)time(NULL));
+  int random;
+  for (int i = 0; i < 2; i++) {
+    LogicDoor door = LogicDoor(rand()%4);
+    switch (door) {
+      case XOR : id += "00"; break;
+      case NOR : id += "01"; break;
+      case NAND : id += "10"; break;
+      case AND : id += "11"; break;
+      default : id += "00"; break;
+    }
+  }
+  return id;
+}
+
+std::string generateBlur() {
+  srand((unsigned)time(NULL));
+  int random;
+  std::string key = "";
+  for (int i = 0; i < 8; i++) {
+    random = rand() % 2;
+    key += std::to_string(random);
+  }
+  return key;
+}
+
+std::string generateKey() {
+  srand((unsigned)time(NULL));
+  int random;
+  std::string key = "";
+  for (int i = 0; i < 25; i++) {
+    random = rand() % 2;
+    key += std::to_string(random);
+  }
+  return key;
+}
+
+std::string generateKeyWithIDAndBlur() {
+    std::string key = generateKey();
+    std::string blur = generateBlur();
+    std::string id = generateID();
+    return id + blur + key;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,80 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "Never gonna give you up";
+const char* password = "n€verG0nnAl€tyoUd0wn";
+
+// MQTT broker details
+const char* mqttServer = "broker.emqx.io";
+const int mqttPort = 1883;
+
+const char* clientId = "VieGehtEsDirMeinFreund"; // Must be unique on the broker
+
+// Topics
+const char* publishTopic = "/DIP/esp8266";
+const char* subscribeTopic = "/DIP/#";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void connectToWiFi() {
+    Serial.print("Connecting to Wi-Fi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to Wi-Fi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+// Function to connect to MQTT broker
+void connectToMQTT() {
+  client.setServer(mqttServer, mqttPort);
+  Serial.print("Connecting to MQTT broker");
+  while (!client.connected()) {
+    if (client.connect(clientId)) {
+      Serial.println("\nConnected to MQTT broker");
+    } else {
+      Serial.print(".");
+    }
+  }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+
+  if (length == 0) {
+    Serial.println("Empty message received. Ignoring.");
+    return;
+  }
+
+  // Check for "Device is alive" message and ignore it
+  if (strncmp((const char*)payload, "01111111110111111101110001110011011101", length) == 0) {
+    Serial.println("Heartbeat message received. Ignoring.");
+    return;
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!client.connected()) {
+    connectToMQTT();
+  }
+  client.loop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Send heartbeat message every 30 seconds
+  static unsigned long lastHeartbeat = 0;
+  if (millis() - lastHeartbeat > 3600000) {
+    client.publish(publishTopic, "01111111110111111101110001110011011101");
+    lastHeartbeat = millis();
+  }
 }
